2c4a67bef8ae4da3437294b71a6f18e7bad709ca
Victor Zenezi <victorzenezi@hotmail.com>
2022-10-22 17:52:42 -0300

    consumo API cadastro usuario

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+  name: string
+  phone: string
+  email: string
+  sector: string
+  role: string
+}
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/model/user.model';
+import { ApiService } from 'src/app/service/api.service';
+import { SnackBarService } from 'src/app/service/snack-bar.service';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +15,30 @@ export class UserComponent implements OnInit {
     nome: new FormControl('', [Validators.required]),
     telefone: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required]),
-    setor: new FormControl('', [Validators.required])
+    setor: new FormControl('', [Validators.required]),
+    funcao: new FormControl('', [Validators.required])
   });
 
-  constructor() { }
+  constructor(private api: ApiService,
+    private snackbar: SnackBarService) { }
 
   ngOnInit(): void {
+  }
+
+  salvar(){
+    let body = new User();
+
+    body.name = this.formUser.controls.nome.value;
+    body.phone = this.formUser.controls.telefone.value;
+    body.email = this.formUser.controls.email.value;
+    body.sector = this.formUser.controls.setor.value;
+    body.role = this.formUser.controls.funcao.value;
+
+    this.api.post("user", body).subscribe( response => {
+      this.snackbar.sucess("Usuário cadastrado com sucesso.")
+    }, error => this.snackbar.error("Erro ao cadastrar usuário."))
+
+    this.formUser.reset();
   }
 
 }

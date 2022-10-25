64c31c0025aa08ce1665fc7022027a98fb5a3da2
Samantha Zenezi <samanthazenezi@gmail.com>
2022-10-25 00:38:11 -0300

    consumo API componente password

--- src/app/pages/password/password.component.ts
+++ src/app/pages/password/password.component.ts
@@ -1,5 +1,8 @@
+import { Password } from './../../model/password.model';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
+import { SnackBarService } from 'src/app/service/snack-bar.service';
 
 @Component({
   selector: 'app-password',
@@ -13,11 +16,25 @@ export class PasswordComponent implements OnInit {
     confirmPassword: new FormControl('', [Validators.required])
   });
 
-  constructor() { }
+  constructor(
+    private api: ApiService,
+    private snackbar: SnackBarService) { }
 
   ngOnInit(): void {
   }
 
-  salvar(){}
+  salvar(){
+    let body = new Password();
+
+    body.password = this.formPassword.controls.password.value;
+    body.confirmPassword = this.formPassword.controls.confirmPassword.value;
+
+    this.api.post("api/user/active", body).subscribe( sucess => {
+      this.snackbar.sucess("Senha cadastrada com sucesso")
+    }, error => this.snackbar.error("Erro ao cadastrar senha.")
+    )
+    
+    this.formPassword.reset();
+  }
 
 }

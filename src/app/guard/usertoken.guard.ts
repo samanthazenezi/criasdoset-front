6138dcf450deb6d1ce0da5308577360f2236b489
victorzenezi <victorzenezi@hotmail.com>
2022-11-02 20:19:51 -0300

    fix guard

--- src/app/guard/usertoken.guard.ts
+++ src/app/guard/usertoken.guard.ts
@@ -16,18 +16,20 @@ export class UsertokenGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      
       let token = route.paramMap.get('token');
-      let retorno = false;
-      this.api.post("user/valid-token", { token: token }).subscribe( sucess => {
-        retorno = true;
+      const result = this.api.post<boolean>("user/valid-token", { token: token })      
+      .subscribe( sucess => {
+        return true;
       }, error => { 
-        retorno = false;
-        this.router.navigateByUrl("erro")
+        return false;
       })
 
-      return retorno
-
+      if (!result) {
+        this.router.navigateByUrl("erro");
+      }
+      else {
+        return true;
+      }
   }
   
 }

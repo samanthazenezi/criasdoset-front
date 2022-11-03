5181a41a6bbd513ca77721a5df97c4b52d6a6414
victorzenezi <victorzenezi@hotmail.com>
2022-11-02 21:11:58 -0300

    card controle de itens

--- src/app/pages/controleitens/controleitens.component.ts
+++ src/app/pages/controleitens/controleitens.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControleitensComponent implements OnInit {
 
+  hidden = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addEmail(){
+    this.hidden = true;
+  }
+
+  enviarEmail(){
+
+  }
+
+  cancelar(){
+    this.hidden = false; 
+
+    // reset form
+  }
+
 
 }
